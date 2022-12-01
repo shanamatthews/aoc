b6c4c4b04cb8e3cda40436fa8dff3440658f00e4
Shana Matthews <shana.l.matthews@gmail.com>
2022-12-01 14:29:23 -0800

    aoc d1p1

---
+++ 2022/day01/a/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+  "fmt"
+  "bufio"
+  "os"
+  "log"
+  "strconv"
+)
+
+func main() {
+  file, _ := os.Open("input.txt")
+  defer file.Close()
+
+  var mostCalories, currentCalories int
+
+  scanner := bufio.NewScanner(file)
+  for scanner.Scan() {
+    var line string = scanner.Text()
+
+    if line == "" {
+      if currentCalories > mostCalories {
+        mostCalories = currentCalories
+      }
+
+      currentCalories = 0
+    } else {
+      lineCals, err := strconv.Atoi(line)
+
+      if err != nil {
+        log.Fatal(err)
+      }
+
+      currentCalories = currentCalories + lineCals
+    }
+  }
+
+  if currentCalories > mostCalories {
+    mostCalories = currentCalories
+  }
+
+  if err := scanner.Err(); err != nil {
+    log.Fatal(err)
+  }
+
+  fmt.Println(mostCalories)
+}
